b829280f5de05d96f3dc1e3245f2cd606db904a0
ay18304025 <ay18304025@ga.tera-house.ac.jp>
2019-02-12 14:16:22 +0900

    SampleSQL_2

---
+++ SampleSQL_2.sql
@@ -0,0 +1,39 @@
+/***********************************
+UserçÏê¨çœÇ›ëOíÒ
+***********************************/
+
+conn maguser/magpass
+
+drop table user_table;
+drop table thread_table;
+drop table res_table;
+
+create table user_table(
+	user_name varchar2(20) PRIMARY KEY,
+	user_password varchar2(20) UNIQUE
+);
+
+create table thread_table(
+	thread_number  number generated always as identity PRIMARY KEY,
+	res_id number(4) UNIQUE,
+	thread_name varchar2(80) NOT NULL UNIQUE,
+	thread_create_time varchar2(4) ,
+	thread_update_time varchar2(4) ,
+	user_name varchar2(20) REFERENCES user_table(user_name),
+	UNIQUE(thread_create_time,thread_update_time)
+);
+
+create table res_table(
+	res_number number generated always as identity PRIMARY KEY,
+	res_id number(4) REFERENCES thread_table(res_id),
+	thread_name varchar2(80) REFERENCES thread_table(thread_name),
+	res_con varchar2(400) NOT NULL,
+	res_cont_time date DEFAULT SYSDATE UNIQUE,
+	res_cont_time2 date DEFAULT SYSDATE UNIQUE,
+	user_name varchar2(20) REFERENCES user_table(user_name)
+);
+
+commit;
+
+
+
