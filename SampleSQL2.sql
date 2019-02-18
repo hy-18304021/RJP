8deb63ceebb164a5c281934d2367dd1c7ad70485
hy-18304021 <yh18304021@ga.tera-house.ac.jp>
2019-02-18 14:11:07 +0900

    no message

--- SampleSQL2.sql
+++ SampleSQL2.sql
@@ -17,10 +17,9 @@ create table thread_table(
 	thread_number  number generated always as identity PRIMARY KEY,
 	res_id number(4) UNIQUE,
 	thread_name varchar2(80) NOT NULL UNIQUE,
-	thread_create_time varchar2(4) ,
-	thread_update_time varchar2(4) ,
+	thread_create_time date DEFAULT SYSDATE UNIQUE,
+	thread_update_time date DEFAULT SYSDATE UNIQUE,
 	user_name varchar2(20) REFERENCES user_table(user_name),
-	UNIQUE(thread_create_time,thread_update_time)
 );
 
 create table res_table(
@@ -29,7 +28,6 @@ create table res_table(
 	thread_name varchar2(80) REFERENCES thread_table(thread_name),
 	res_con varchar2(400) NOT NULL,
 	res_cont_time date DEFAULT SYSDATE UNIQUE,
-	res_cont_time2 date DEFAULT SYSDATE UNIQUE,
 	user_name varchar2(20) REFERENCES user_table(user_name)
 );
 
